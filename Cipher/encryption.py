3251163031424a87b820ead35acf6eb0bfb0684b
tkv337 <arshia.to@hotmail.com>
2023-08-23 12:21:15 -0400

    new stuff added today

---
+++ Cipher/encryption.py
@@ -0,0 +1,32 @@
+import random
+import string
+
+chars = " " + string.punctuation + string.digits + string.ascii_letters
+
+chars = list(chars)
+
+key = chars.copy()
+
+random.shuffle(key)
+
+#ENCRYPT
+plain_text = input("enter a message you want to encrypt: ")
+cipher_text = ""
+
+for letter in plain_text:
+    index = chars.index(letter)
+    cipher_text += key[index]
+
+print(f"orginal message : {plain_text}")
+print(f"encrypted message : {cipher_text}")
+
+#DECRYPT
+cipher_text= input("enter a message you want decrypted: ")
+plain_text = ""
+
+for letter in cipher_text:
+    index = key.index(letter)
+    plain_text += chars[index]
+
+print(f"encrypted message : {cipher_text}")
+print(f"orginal message : {plain_text}")
